e0062450820add9c33804e0438e593ecbfdcc500
Karim <karimelgaili1014@gmail.com>
2026-01-20 21:25:22 -0500

    homework 1

---
+++ readme.txt
@@ -0,0 +1,23 @@
+The document above may be revised... any substantive changes will be announced in class.
+
+The location in Gradescope to submit the homework will be published next week. Note the deadline is 5 PM on 1/23. No late work is accepted.
+
+In IntelliJ, create a new project named Homework 1 to house this work. The program should start by running a class called Main. Your project should have a folder for src, a folder for docs, and a folder for tests.
+
+You may have other questions... bring them to class next Tuesday.
+Given the sonnet below, write a program that writes out the sonnet and stops at a random word, printing a series of underscores instead, and not printing the rest of the sonnet. The program should prompt the user for the next word. Once the user responds, the program should indicate correct or error, and restart the sonnet, stopping at a different word. Program continues until the user gets three words correct or three words incorrect.
+
+Two households, both alike in dignity,
+In fair Verona, where we lay our scene,
+From ancient grudge break to new mutiny,
+Where civil blood makes civil hands unclean.
+From forth the fatal loins of these two foes
+A pair of star-cross’d lovers take their life;
+Whose misadventured piteous overthrows
+Do with their death bury their parents’ strife.
+The fearful passage of their death-mark’d love,
+And the continuance of their parents’ rage,
+Which, but their children’s end, nought could remove,
+Is now the two hours’ traffic of our stage;
+The which if you with patient ears attend,
+What here shall miss, our toil shall strive to mend.
---
+++ src/Main.java
@@ -0,0 +1,86 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+void main() {
+    //sonnet
+    int correct = 0;
+    int count = 0;
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<Character> sonnetList = new ArrayList<>();
+    String paragraph = "Two households, both alike in dignity,\n" +
+            "In fair Verona, where we lay our scene,\n" +
+            "From ancient grudge break to new mutiny,\n" +
+            "Where civil blood makes civil hands unclean.\n" +
+            "From forth the fatal loins of these two foes\n" +
+            "A pair of star-cross’d lovers take their life;\n" +
+            "Whose misadventured piteous overthrows\n" +
+            "Do with their death bury their parents’ strife.\n" +
+            "The fearful passage of their death-mark’d love,\n" +
+            "And the continuance of their parents’ rage,\n" +
+            "Which, but their children’s end, nought could remove,\n" +
+            "Is now the two hours’ traffic of our stage;\n" +
+            "The which if you with patient ears attend,\n" +
+            "What here shall miss, our toil shall strive to mend.\n";
+    while (count < 3 && correct == 0) {
+        Random rand = new Random();
+        int i = rand.nextInt(paragraph.length());
+        while (paragraph.charAt(i) == '\n' || paragraph.charAt(i) == ' ' || paragraph.charAt(i) == '.' || paragraph.charAt(i) == ',' || paragraph.charAt(i) == ';') {
+            i = rand.nextInt(paragraph.length()); //generate random num
+
+        }
+        int lowIndex = i;
+        int highIndex = i;
+        int sizeOfWord = 0;
+        while (i != 0 && (paragraph.charAt(lowIndex) != '\n' && paragraph.charAt(lowIndex) != ' ' && paragraph.charAt(lowIndex) != '.' && paragraph.charAt(lowIndex) != ',' && paragraph.charAt(lowIndex) != ';')) {
+            lowIndex -= 1; //get left side index of word
+
+        }
+        if (lowIndex != i) { //exception case if i = 0
+            lowIndex += 1;
+        }
+        while (i != (paragraph.length() - 1) && (paragraph.charAt(highIndex) != '\n' && paragraph.charAt(highIndex) != ' ' && paragraph.charAt(highIndex) != '.' && paragraph.charAt(highIndex) != ',' && paragraph.charAt(highIndex) != ';')) {
+            highIndex += 1; //get right side index of word
+        }
+        highIndex -= 1;
+        sizeOfWord = (highIndex + 1) - lowIndex;
+        for (int j = lowIndex; j < (highIndex + 1); j++) {
+            sonnetList.add(paragraph.charAt(j));
+             //add word to arraylist
+        }
+        String word = "";
+        for (char character : sonnetList) {
+            word += character; //the word you are trying to guess
+        }
+        sonnetList.clear();
+        for (int o = 0; o < paragraph.length(); o++){
+            sonnetList.add(paragraph.charAt(o)); //add paragraph characters to arraylist
+        }
+        for (int p = lowIndex; p < (highIndex + 1); p++){
+            sonnetList.set(p, '_'); //change that word to underscore
+        }
+        for (char cha : sonnetList) {
+            System.out.print(cha);
+        }
+
+        System.out.println("Guess what the missing word is.");
+        String guess = scanner.nextLine();
+        System.out.println("Your guess was: " + guess + " and the word was " + word);
+        if (guess.equals(word)){
+            correct = 1;
+        }
+        else{
+            count += 1;
+        }
+    }
+
+
+    if( correct == 1){
+        System.out.println("Great Job.");
+    }
+    else{
+        System.out.println("Your three guesses were wrong.");
+    }
+}
+
